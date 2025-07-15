91d496868a9d0a6c45a64df39aba83a4f62773e0
mmgbeojiri <150837937+mmgbeojiri@users.noreply.github.com>
2025-07-15 14:33:25 +0000

    added nav bar

---
+++ app/navbar.tsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <div className="flex w-screen flex shadow-sm justify-between px-5 h-15 gap-4 items-center flex-row">
+      <h1 className="!py-1 !px-2">Flixhub</h1>
+      <div className="flex-row flex w-screen ">
+      <input type="text" className="flex-9 !border-r-0 !rounded-r-none" placeholder="Search" />
+      <input type="submit" className="flex-1 !border-l-0 !rounded-l-none !bg-blue-500 !text-slate-50" value="Search"/>
+      </div>
+      </div>
+  );
+}
