19d79b6b2bddc47938b5e49e0a0bad0f74520f70
mmgbeojiri <150837937+mmgbeojiri@users.noreply.github.com>
2025-07-15 14:23:04 +0000

    searchbar

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ export default function Home() {
   return (
     <div className="flex w-screen h-screen justify-center items-center flex-col">
       <h1>Flixhub</h1>
-      <div className="flex-col w-screen">
-      <input type="text" className="flex-9" placeholder="Search" />
-      <input type="submit" value="Search"/>
+      <div className="flex-row flex w-screen p-10">
+      <input type="text" className="flex-9 !border-r-0 !rounded-r-none" placeholder="Search" />
+      <input type="submit" className="flex-1 !border-l-0 !rounded-l-none !bg-blue-500 !text-slate-50" value="Search"/>
       </div>
       </div>
   );
