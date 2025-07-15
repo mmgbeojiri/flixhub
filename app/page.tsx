3479b349d38e190a2d412926d5a9b6f857d69f94
mmgbeojiri <150837937+mmgbeojiri@users.noreply.github.com>
2025-07-15 13:44:32 +0000

    added searchbar

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Flixhub</div>
+    <div className="flex w-screen h-screen justify-center items-center flex-col">
+      <h1>Flixhub</h1>
+      <div className="flex-col w-screen">
+      <input type="text" className="flex-9" placeholder="Search" />
+      <input type="submit" value="Search"/>
+      </div>
+      </div>
   );
 }
