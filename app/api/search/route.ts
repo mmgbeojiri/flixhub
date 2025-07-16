cb00034f232d6553ad324f10f6ddd486e7e7eec5
mmgbeojiri <150837937+mmgbeojiri@users.noreply.github.com>
2025-07-16 13:09:51 +0000

    added routes

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,7 +1,9 @@
     // app/api/users/route.ts
     import { NextResponse } from 'next/server';
     import type { NextRequest } from 'next/server';
-
+    interface requestBody {
+      url: string;
+    }
     export async function GET(request: NextRequest) {
       // Handle GET requests to /api/users
       // Access request data (e.g., query parameters, headers) from 'request'
@@ -12,10 +14,10 @@
 
     export async function POST(request: NextRequest) {
       // Handle POST requests to /api/users
-      const body = await request.json(); // Parse request body as JSON
+      const { url }: requestBody = await request.json(); // Parse request body as JSON
 
       // Perform backend logic (e.g., save data to a database)
-      console.log('Received data:', body);
+      console.log('Received data:', url);
 
-      return NextResponse.json({ message: 'User created successfully', data: body });
+      return NextResponse.json({ message: 'Message got created successfully', data: url });
     }
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,22 @@
+"use client";
 import Image from "next/image";
+import axios from "axios";
 
+function SendPostRequest(url: string) {
+  let plusUrl: string = url.replaceAll(" ", "+");
+  axios.post('/api/search', { "url": plusUrl });
+}
 export default function Home() {
   return (
     <div className="flex w-screen h-screen justify-center items-center flex-col">
       <h1>Flixhub</h1>
       <div className="flex-row flex w-screen p-10">
-      <input type="text" className="flex-9 !border-r-0 !rounded-r-none" placeholder="Search" />
-      <input type="submit" className="flex-1 !border-l-0 !rounded-l-none !bg-blue-500 !text-slate-50" value="Search"/>
+      <input type="text" onSubmit={
+        (e) => {
+          e.preventDefault();
+          SendPostRequest(e.target.value);
+        }} className="flex-9 !border-r-0 !rounded-r-none" placeholder="Search" />
+      <input type="submit"className="flex-1 !border-l-0 !rounded-l-none !bg-blue-500 !text-slate-50" value="Search"/>
       </div>
       </div>
   );
