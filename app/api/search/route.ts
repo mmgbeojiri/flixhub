bd353230bca4fc2cbf235f87525b334dfe8bef70
mmgbeojiri <150837937+mmgbeojiri@users.noreply.github.com>
2025-07-16 13:29:21 +0000

    webscraping

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,23 +1,49 @@
-    // app/api/users/route.ts
-    import { NextResponse } from 'next/server';
-    import type { NextRequest } from 'next/server';
-    interface requestBody {
-      url: string;
-    }
-    export async function GET(request: NextRequest) {
-      // Handle GET requests to /api/users
-      // Access request data (e.g., query parameters, headers) from 'request'
+// app/api/users/route.ts
+import { url } from 'inspector';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import * as cheerio from 'cheerio'; 
+import axios from 'axios';
+interface requestBody {
+  url: string;
+}
+interface Entry {
+    text: string;
+    link: string;
+    image: string;
+}
+const urlPrefix: string = "https://lookmovie.onl/?s="
+// We use .onl because .to is blocked on school networks.
+let searchUrl: string = "";
 
-      // Return a Next.js Response object
-      return NextResponse.json("ahhhh");
-    }
+export async function GET(request: NextRequest) {
+  // Handle GET requests to /api/users
+  // Access request data (e.g., query parameters, headers) from 'request'
 
-    export async function POST(request: NextRequest) {
-      // Handle POST requests to /api/users
-      const { url }: requestBody = await request.json(); // Parse request body as JSON
+  // Return a Next.js Response object
+  return NextResponse.json("ahhhh");
+}
 
-      // Perform backend logic (e.g., save data to a database)
-      console.log('Received data:', url);
+export async function POST(request: NextRequest) {
+  // Handle POST requests to /api/users
+  const { url }: requestBody = await request.json(); // Parse request body as JSON
+  searchUrl = urlPrefix + url;
+  // Perform backend logic (e.g., save data to a database)
+  console.log('Received data:', searchUrl);
 
-      return NextResponse.json({ message: 'Message got created successfully', data: url });
-    }
+  axios.get(searchUrl).then((response) => {
+    $ = cheerio.load(response.data);
+
+    let entries: Entry[] = [];
+    $("#main article").each((index, element) => {
+      let newEntry: Entry = {
+        text: $(element).find(".entry-title").text() || "",
+        link: $(element).find("a").attr("href") || "",
+        image: $(element).find("div img").attr("src") || ""
+      }
+      entries.push(newEntry);
+      console.log(newEntry);
+    })
+
+  return NextResponse.json({ message: 'Message got created successfully', data: url });
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,26 @@
 "use client";
 import Image from "next/image";
+import { useState } from "react";
 import axios from "axios";
+
+const API_URL:string = '/api/search';
 
 function SendPostRequest(url: string) {
   let plusUrl: string = url.replaceAll(" ", "+");
-  axios.post('/api/search', { "url": plusUrl });
+  axios.post(API_URL, { "url": plusUrl });
 }
 export default function Home() {
+  const [inputText, setInputText] = useState<string>("");
   return (
     <div className="flex w-screen h-screen justify-center items-center flex-col">
       <h1>Flixhub</h1>
       <div className="flex-row flex w-screen p-10">
-      <input type="text" onSubmit={
+      <input type="text" onChange={(e) => setInputText(e.target.value)} className="flex-9 !border-r-0 !rounded-r-none" placeholder="Search" />
+      <input type="submit" onClick={
         (e) => {
           e.preventDefault();
-          SendPostRequest(e.target.value);
-        }} className="flex-9 !border-r-0 !rounded-r-none" placeholder="Search" />
-      <input type="submit"className="flex-1 !border-l-0 !rounded-l-none !bg-blue-500 !text-slate-50" value="Search"/>
+          SendPostRequest(inputText);
+        }} className="flex-1 !border-l-0 !rounded-l-none !bg-blue-500 !text-slate-50" value="Search"/>
       </div>
       </div>
   );
